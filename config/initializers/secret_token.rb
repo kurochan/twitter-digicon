f7c1d2dd2938a9569a38464c3cf241275f4fad50
Kazuya Takeshima <mail@mitukiii.jp>
2010-12-26 17:39:30 +0900

    Rails3 Twitter initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails3Twitter::Application.config.secret_token = 'e279ed0d61e2f0ba7015a395a32b7125b5590be37ddbe39de42631f659d382ca9ecb2f8accb5d44bc40a6ce79db193a0ddeddcd902186fdf7dc875b3c546b7b0'
