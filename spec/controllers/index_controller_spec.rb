33a1011c475ca287fa6c465a7206c0e51478cf5c
Kazuya Takeshima <mail@mitukiii.jp>
2010-12-26 18:39:23 +0900

    Change callback action spec to redirect from success.

--- spec/controllers/index_controller_spec.rb
+++ spec/controllers/index_controller_spec.rb
@@ -2,25 +2,19 @@ require 'spec_helper'
 
 describe IndexController do
 
-  describe "GET 'index'" do
-    it "should be successful" do
-      get 'index'
-      response.should be_success
-    end
+  describe 'GET "index"' do
+    before { get :index }
+    it { response.should be_success }
   end
 
-  describe "GET 'oauth'" do
-    it "should be successful" do
-      get 'oauth'
-      response.should be_success
-    end
+  describe 'GET "oauth"' do
+    before { get :oauth }
+    it { response.should be_redirect }
   end
 
-  describe "GET 'callback'" do
-    it "should be successful" do
-      get 'callback'
-      response.should be_success
-    end
+  describe 'GET "callback"' do
+    before { get :callback }
+    it { response.should be_redirect }
   end
 
 end
