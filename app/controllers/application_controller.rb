833ac7bb76df7f63697594a65f748da50b28835e
Kazuya Takeshima <mail@mitukiii.jp>
2010-12-26 20:16:10 +0900

    Add OAuth::consumer wrapper method consumer to applicaion controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  protected
+  def self.consumer
+    OAuth::Consumer.new(
+      'consumer key',
+      'consumer secret',
+      {site: "http://twitter.com"}
+    )
+  end
+
 end
