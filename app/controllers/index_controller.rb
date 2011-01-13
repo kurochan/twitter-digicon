2a13e59e50a33db8f4741a73c78570fefdfc458d
Kazuya Takeshima <mail@mitukiii.jp>
2011-01-14 00:44:18 +0900

    Add oauth action.

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -3,7 +3,15 @@ class IndexController < ApplicationController
   end
 
   def oauth
-    redirect_to :index
+    callback_url = "http://#{request.host_with_port}/callback"
+    request_token = self.class.consumer.get_request_token(oauth_callback: callback_url)
+
+    session[:request_token] = {
+      token: request_token.token,
+      secret: request_token.secret
+    }
+
+    redirect_to request_token.authorize_url
   end
 
   def callback
