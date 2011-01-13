55b99a013d5230e546f0bd4aed9b290716916e0e
Kazuya Takeshima <mail@mitukiii.jp>
2011-01-14 00:44:39 +0900

    Add callback action.

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -15,6 +15,25 @@ class IndexController < ApplicationController
   end
 
   def callback
+    request_token = OAuth::RequestToken.new(
+      self.class.consumer,
+      session[:request_token][:token],
+      session[:request_token][:secret]
+    )
+
+    access_token = request_token.get_access_token(
+      {},
+      oauth_token: params[:oauth_token],
+      oauth_verifier: params[:oauth_verifier]
+    )
+
+    session.delete :request_token
+
+    session[:oauth] = {
+      token: access_token.token,
+      secret: access_token.secret
+    }
+
     redirect_to :index
   end
 
