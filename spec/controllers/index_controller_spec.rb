a9b727ed3ef6abd70c99724514519a52112510fd
Kazuya Takeshima <mail@mitukiii.jp>
2011-01-14 02:45:40 +0900

    Add OAuth classes stub to callback spec.

--- spec/controllers/index_controller_spec.rb
+++ spec/controllers/index_controller_spec.rb
@@ -13,7 +13,24 @@ describe IndexController do
   end
 
   describe 'GET "callback"' do
-    before { get :callback }
+    before do
+      session[:request_token] = {
+        token: 'request_token',
+        secret: 'request_secret'
+      }
+
+      request_token = stub(OAuth::RequestToken)
+      access_token = stub(OAuth::AccessToken)
+
+      OAuth::RequestToken.stub(:new).and_return(request_token)
+
+      request_token.stub(:get_access_token).and_return(access_token)
+
+      access_token.stub(:token).and_return('access_token')
+      access_token.stub(:secret).and_return('access_secret')
+
+      get :callback, oauth_token: 'oauth_token', oauth_verifier: 'oauth_verifier'
+    end
     it { response.should be_redirect }
   end
 
